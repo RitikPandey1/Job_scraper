ea3064538632faee804f84f578e2f794fcabf5fc
RitikPandey1 <rp18.pandey@gmail.com>
2022-06-23 11:12:45 +0530

    ui update

--- frontend/src/Components/Result.js
+++ frontend/src/Components/Result.js
@@ -37,13 +37,13 @@ function Result({res}){
                     <div className="border-2 p-4" key={i}>
                     <h1 className="truncate font-bold">{c.Job_Title}</h1>
                     <h3 className="truncate  truncate" >{c.Company_Name}</h3>
-                    <h4>Experience : {c.Experience}</h4>
-                    <h4>Location : {c.Location}</h4>
-                    <h4>Salary : {c.Salary}</h4>
-                    <h4>Key Skills : {c.skills.map(s=>s+", ")} </h4> 
-                    <a href={c.Job_post_link} target="_blank" rel="noreferrer">
+                    <h4><span className="text-teal-700">Experience</span> : {c.Experience}</h4>
+                    <h4><span  className="text-teal-700">Location</span> : {c.Location}</h4>
+                    <h4><span className="text-teal-700">Salary</span> : {c.Salary === ""?"Not disclosed":c.Salary}</h4>
+                    <h4> <span className="text-teal-700" >Key Skills</span> : {c.skills.map(s=>s+", ")} </h4> 
+                  <a href={c.Job_post_link} target="_blank" rel="noreferrer">
                   <button className="bg-teal-500 p-2 text-white hover:bg-teal-700 rounded mt-4">Apply Now</button>
-                    </a> 
+                    </a>  
                 </div>
                     )
                 })} 
